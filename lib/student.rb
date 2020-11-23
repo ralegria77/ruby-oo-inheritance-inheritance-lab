99ef9eee76642bc1bdb174a490be675c16f63121
rebec <ralegria77@gmail.com>
2020-11-22 17:19:21 -0800

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(string)
+        @knowledge << string.concat
+    end
+
+    def knowledge
+        @knowledge
+    end
+
 
 end
